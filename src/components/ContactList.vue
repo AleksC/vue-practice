3bb3adebbb5d06c73e204334027a1f2f36733ac2
AleksC <aleksandar.cosic1@yahoo.com>
2018-08-06 20:09:59 +0200

    2. zadatak

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="hello">
       <h1>Contact List</h1>
+      <table border="1">
+          <thead>
+              <th>Name</th>
+              <th>email</th>
+          </thead>
+          <tbody>
+              <tr v-for="(contact, key) in contacts" :key="key">
+              <td> {{ contact.name }} </td>
+              <td> {{ contact.email }} </td>
+              </tr>
+          </tbody>
+      </table>
   </div>
 </template>
 
 <script>
 export default {
   name: "ContactList",
-  props: {
-    msg: String
+  data() {
+    return {
+      contacts: [
+        { name: "George", email: "george@bluthcompany.com" },
+        { name: "George Michael", email: "georgemichael@bluthcompany.com" },
+        { name: "Michael", email: "michael@bluthcompany.com" },
+        { name: "Buster", email: "buster@bluthcompany.com" }
+      ]
+    };
   }
 };
 </script>
